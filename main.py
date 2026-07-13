b789fe73ee7eef669824e1fb2b405192632ad8bf
Paulien van Slingerland <paulien@observingminds.com>
2026-07-13 08:31:25 -0400

    formatted the output (task 3)

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from emotion_detection import emotion_detector
 
 
-def main(): 
+def main2b(): 
     result = emotion_detector("I love this new technology.")
 
     with open("2b_application_creation.txt", "w") as file:
         file.write(result)
 
+def main3(): 
+    result = emotion_detector("I am so happy I am doing this.")
+    with open("3b_formatted_output_test.txt", "w") as file:
+        file.write(str(result))
+
 if __name__ == '__main__':
-    main()
+    main3()
